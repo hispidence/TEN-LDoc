b41a3bf7976491f9a6f689a9b159e9e4da392bff
hispidence <squidshirehimself@gmail.com>
2022-12-09 21:13:29 +0000

    Add ability to have arbitrary blocks of syntax-highlighted code outside of a "usage" tag with a new tag, "advancedDesc".

--- ldoc/doc.lua
+++ ldoc/doc.lua
@@ -23,7 +23,7 @@ local TAG_MULTI,TAG_ID,TAG_SINGLE,TAG_TYPE,TAG_FLAG,TAG_MULTI_LINE = 'M','id','S
 --  - 'N' tags which have no associated value, like 'local` (TAG_FLAG)
 --  - 'T' tags which represent a type, like 'function' (TAG_TYPE)
 local known_tags = {
-   param = 'M', see = 'M', comment = 'M', usage = 'ML', ['return'] = 'M', field = 'M', author='M',set='M';
+   param = 'M', see = 'M', comment = 'M', advancedDesc = 'ML', usage = 'ML', ['return'] = 'M', field = 'M', author='M',set='M';
    class = 'id', name = 'id', pragma = 'id', alias = 'id',
    copyright = 'S', summary = 'S', description = 'S', release = 'S', license = 'S',
    fixme = 'S', todo = 'S', warning = 'S', raise = 'S', charset = 'S', within = 'S',
@@ -613,6 +613,7 @@ function Item:finish()
    self.type = read_del(tags,'class')
    self.modifiers = extract_tag_modifiers(tags)
    self.usage = read_del(tags,'usage')
+   self.advancedDesc = read_del(tags,'advancedDesc')
    tags.see = read_del(tags,'see')
    if tags.see then
       tags.see = tools.identifier_list(tags.see)
@@ -622,6 +623,40 @@ function Item:finish()
          local usage = self.usage[i]:gsub('^%s*\n','')
          self.usage[i] = text.dedent(usage)
       end
+   end
+
+   if self.advancedDesc then
+	   self.advancedDesc.lines = {}
+	   self.advancedDesc.formatLines = {}
+	   for i = 1,#self.advancedDesc do
+
+		   local currentBlock = 0
+		   local advancedDesc = self.advancedDesc[i]
+		   advancedDesc = text.dedent(advancedDesc)
+		   local prevIsCode = false
+		   local start = true
+
+		   for line in advancedDesc:gmatch("[^\n]*\n?") do
+			   if line:find("^\t") or (prevIsCode and line:find("^\n$")) then
+				   if not prevIsCode then
+					   currentBlock = currentBlock + 1
+					   self.advancedDesc.lines[currentBlock] = ""
+					   self.advancedDesc.formatLines[currentBlock] = true
+				   end
+				   self.advancedDesc.lines[currentBlock] = self.advancedDesc.lines[currentBlock] .. line:gsub("^\t", "")
+				   prevIsCode = true
+			   else
+				   if prevIsCode or start then
+					   currentBlock = currentBlock + 1
+					   self.advancedDesc.lines[currentBlock] = ""
+					   self.advancedDesc.formatLines[currentBlock] = false
+				   end
+				   self.advancedDesc.lines[currentBlock] = self.advancedDesc.lines[currentBlock] .. line
+				   prevIsCode = false
+			   end
+			   start = false
+		   end
+	   end
    end
    if  doc.project_level(self.type) then
       -- we are a module, so become one!
